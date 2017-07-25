ff617c31e65d3a582a80a450c58d858a2b5f8eb2
Michael Goderbauer <goderbauer@google.com>
2017-07-25 00:40:55 -0700

    Fail if dartfmt exits with error (#9)

--- lib/src/format_command.dart
+++ lib/src/format_command.dart
@@ -95,13 +95,8 @@ class FormatCommand extends PluginCommand {
   Future<Null> _formatDart() async {
     print('Formatting all .dart files...');
     final Iterable<String> dartFiles = await _getFilesWithExtension('.dart');
-    // TODO(goderbauer): add "exitOnError: true" after fix for
-    //     https://github.com/dart-lang/dart_style/issues/522 is released.
-    await runAndStream(
-      'flutter',
-      <String>['format']..addAll(dartFiles),
-      workingDir: packagesDir, /* exitOnError: true */
-    );
+    await runAndStream('flutter', <String>['format']..addAll(dartFiles),
+        workingDir: packagesDir, exitOnError: true);
   }
 
   Future<List<String>> _getFilesWithExtension(String extension) async =>
